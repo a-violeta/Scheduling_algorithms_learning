d95f009345b6cf15d29e841a1209751915c602ac
a-violeta <iulia.violeta.avram@gmail.com>
2026-03-10 22:25:08 +0200

    Basic interface

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 from fastapi import FastAPI
 from routers.scheduling import router as scheduling_router
 
 app = FastAPI()
 app.include_router(scheduling_router)
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
-def home():
-    return {"status": "server running"}
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
